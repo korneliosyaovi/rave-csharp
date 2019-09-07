1ad1cd9171c29117b9deeb97a94c45ccfd3b42b7
Official-kornelios <korneliosyaovi@gmail.com>
2019-09-07 11:17:16 +0100

    Changed chargeResponse location from data to models.charge

--- Rave/api/RaveRequest.cs
+++ Rave/api/RaveRequest.cs
@@ -1,29 +1,29 @@
 ﻿using System.Net.Http;
 using System.Net.Http.Headers;
-using Rave.data;
+using Rave.Models.Charge;
 
 namespace Rave.api
 {
     internal class RaveRequest : RaveRequestBase<RaveResponse<ChargeResponse>, ChargeResponse>
     {
-       
+
     }
 
     internal class RaveRequest<T1, T2> : RaveRequestBase<T1, T2> where T1 : RaveResponse<T2>, new() where T2 : class
     {
-        internal RaveRequest():base()
+        internal RaveRequest() : base()
         {
             Config = new RaveConfig(false);
             HttpClient.DefaultRequestHeaders.Accept.Clear();
             HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
-        internal RaveRequest(RaveConfig config): base(config)
+        internal RaveRequest(RaveConfig config) : base(config)
         {
             Config = config;
             HttpClient.DefaultRequestHeaders.Accept.Clear();
             HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-       
+
     }
 }
